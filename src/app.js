7a65e24fadbec46d99a42c45d362fe2151bdf05f
tombel <tomasbelardo@gmail.com>
2022-06-06 23:25:52 -0300

    lo que hicimos en la clase del miercoles

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mainRoutes = require('./routes/mainRoutes')
+const path=require("path")
 
 app.use(express.static(__dirname + '/public'));
-app.set('view engine','ejs'),
+app.set('view engine','ejs');
+app.set("views",path.join(__dirname,"/views"));
 
 app.listen(PORT, ()=>{
     console.log(`Server running ar port ${PORT}`);
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const fs=require("fs");
+const dbProductos=path.join(__dirname, "../database/productos.json")
+const readJsonFile= path=>{
+    const data=fs.readFileSync(path, "utf-8")
+    const dataParse=JSON.parse(data)
+    return dataParse;
+}
 
 const controller = {
     index: (req, res) => {
-        res.render("home");
+        const productos = readJsonFile(dbProductos);
+        res.render("home",{productos:productos});
     },
+
     about: (req, res) => {
         res.render(path.join("about"));
     },

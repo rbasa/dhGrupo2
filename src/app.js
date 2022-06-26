cbd20c1c0abf697ae739941d505c8980801e5141
rbasa <=>
2022-06-26 13:21:20 -0300

    consistencia en el uso de comillas

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
-app.set("views",path.join(__dirname,"/views"));
+app.set('views',path.join(__dirname,'/views'));
 app.listen(PORT, ()=>{
     console.log(`Server running ar port ${PORT}`);
 });
 
 
-app.use("/", mainRoutes);
-app.use("/products", productRoutes);
-app.use("/login", mainRoutes);
-app.use("/register", mainRoutes);
-app.use("/cart", mainRoutes);
+app.use('/', mainRoutes);
+app.use('/products', productRoutes);
+app.use('/login', mainRoutes);
+app.use('/register', mainRoutes);
+app.use('/cart', mainRoutes);
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,9 @@
-const path = require("path");
-const fs = require("fs");
-const dbProductos = path.join(__dirname, "../database/productos.json");
-const dbCart = path.join(__dirname, "../database/cart.json");
+const path = require('path');
+const fs = require('fs');
+const dbProductos = path.join(__dirname, '../database/productos.json');
+const dbCart = path.join(__dirname, '../database/cart.json');
 const readJsonFile = (path) => {
-  const data = fs.readFileSync(path, "utf-8");
+  const data = fs.readFileSync(path, 'utf-8');
   const dataParse = JSON.parse(data);
   return dataParse;
 };
@@ -11,10 +11,10 @@ const readJsonFile = (path) => {
 const controller = {
   index: (req, res) => {
     const productos = readJsonFile(dbProductos);
-    res.render("home", { productos: productos });
+    res.render('home', { productos: productos });
   },
   about: (req, res) => {
-    res.render(path.join("about"));
+    res.render(path.join('about'));
   },
   cart: (req, res) => {
     const seleccionados = readJsonFile(dbCart);
@@ -22,16 +22,16 @@ const controller = {
     seleccionados.forEach(e => {
       total = total + e.precio*e.cantidad;
     })
-    res.render("products/cart",{ seleccionados: seleccionados, total:total });
+    res.render('products/cart',{ seleccionados: seleccionados, total:total });
   },
   login: (req, res) => {
-    res.render("users/login");
+    res.render('users/login');
   },
   register: (req, res) => {
-    res.render("users/register");
+    res.render('users/register');
   },
   create: (req, res) => {
-    res.render("/create");
+    res.render('/create');
   },
 };
 module.exports = controller;

896050f9e2981388c0c989bdcea89336b39cb9c6
tombel <tomasbelardo@gmail.com>
2022-09-29 19:12:12 -0300

    se agrega la capacidad de subir imagenes a la carpeta images desde multer

--- src/middlewares/multerUsers.js
+++ src/middlewares/multerUsers.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname,'../public/images/users'))
+    cb(null, path.join(__dirname,'../public/images'))
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
